3b18c157c279427801dc8f5c5a37a4208223e0fd
Rafael FS <rafesilva@gmail.com>
2018-05-25 12:14:57 +1000

    name_corrections

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,11 @@
 class Author < ApplicationRecord
-	has_many :books
+  has_many :books
+ 
+  after_save :index_books_in_elasticsearch
+ 
+  private
+ 
+  def index_books_in_elasticsearch
+    books.find_each { |book| book.__elasticsearch__.index_document }
+  end
 end
